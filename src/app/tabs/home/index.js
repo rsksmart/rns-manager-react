1dfab0a4d4963749a2a785d4f0404cfb3c00b9db
Ilan Olkies <ilanolkies@outlook.com>
2019-02-25 18:59:50 -0300

    Add landing links

--- src/app/tabs/home/index.js
+++ src/app/tabs/home/index.js
@@ -1,6 +1,33 @@
 import React, { Component } from 'react';
 import { SearchDomainContainer, ResolveAddressContainer } from './containers';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Card } from 'react-bootstrap';
+
+const admin = (
+  <Card>
+    <Card.Body>
+      <Card.Title>Admin your domain</Card.Title>
+      <Card.Text>
+        Set your resolution address<br />
+        Create subdomains, admin them too.
+      </Card.Text>
+      <Card.Link href="/admin">Admin</Card.Link>
+    </Card.Body>
+  </Card>
+);
+
+const external = (
+  <Card>
+    <Card.Body>
+      <Card.Title>Learn about RNS</Card.Title>
+      <Card.Text>
+        Be part of the internet of value revolution.
+      </Card.Text>
+      <Card.Link href="https://rifos.org">RIF OS</Card.Link>
+      <Card.Link href="https://docs.rns.rsk.co">RNS Docs</Card.Link>
+    </Card.Body>
+  </Card>
+)
+
 
 class Home extends Component {
   render () {
@@ -11,7 +38,17 @@ class Home extends Component {
             <Col><SearchDomainContainer /></Col>
           </Row>
           <Row>
+            <Col>
+              <h1>get your domain,<br />forget your address</h1>
+            </Col>
+            <Col>
+              img
+            </Col>
+          </Row>
+          <Row>
+            <Col>{admin}</Col>
             <Col><ResolveAddressContainer /></Col>
+            <Col>{external}</Col>
           </Row>
         </Container>
       </div>
