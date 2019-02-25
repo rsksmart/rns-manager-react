d1d44d3386f9f954ce8a5ffc8f3674ff50a754c3
Ilan Olkies <ilanolkies@outlook.com>
2019-02-25 03:17:42 -0300

    Add bootstrap to resolve address

--- src/app/containers/ResolveAddressContianer.js
+++ src/app/containers/ResolveAddressContianer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { resolveAddress } from '../operations';
+import { Form, InputGroup, FormControl, Button } from 'react-bootstrap';
 
 const mapStateToProps = state => ({
   address: state.address,
@@ -15,13 +16,18 @@ const ResolveAddressContainer = ({
   let input;
 
   return (
-    <form onSubmit={e => {
+    <Form onSubmit={e => {
       e.preventDefault();
       dispatch(resolveAddress(input.value));
     }}>
-      <input ref={node => (input = node)} />
+      <InputGroup>
+        <FormControl ref={node => (input = node)} />
+        <InputGroup.Append>
+          <Button type="submit">Resolve</Button>
+        </InputGroup.Append>
+      </InputGroup>
       <p>{resolveAddressLoading ? 'Loading...' : address}</p>
-    </form>
+    </Form>
   );
 }
 
