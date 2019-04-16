804ef47b4d4a56f00817c5dbcd2e7f8b514c6fe2
Ilan Olkies <ilanolkies@outlook.com>
2019-04-16 15:58:03 -0300

    Display checksummed address in auth modal

--- src/app/auth/containers/AuthModalContainer.js
+++ src/app/auth/containers/AuthModalContainer.js
@@ -3,13 +3,14 @@ import { closeModal } from '../actions';
 import { authenticate } from '../operations';
 import { connect } from 'react-redux';
 import { networkSelector } from '../selectors';
+import { toChecksumAddress } from '../../selectors';
 
 const mapStateToProps = state => ({
   show: state.auth.showModal,
   hasMetamask: state.auth.hasMetamask,
   enabling: state.auth.enabling,
   enableError: state.auth.enableError,
-  address: state.auth.address,
+  address: state.auth.address && toChecksumAddress(state)(state.auth.address),
   network: networkSelector(state.auth.network),
   authError: state.auth.authError,
   domain: state.auth.domain,
