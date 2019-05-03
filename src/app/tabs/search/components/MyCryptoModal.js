d1d85cf59493e4edc0127e8b550bd32973c69e8f
Ilan Olkies <ilanolkies@outlook.com>
2019-05-03 17:27:50 -0300

    i18n in search mycrypto modal

--- src/app/tabs/search/components/MyCryptoModal.js
+++ src/app/tabs/search/components/MyCryptoModal.js
@@ -1,59 +1,68 @@
 import React from 'react';
-import { Modal, Table } from 'react-bootstrap';
+import { Modal, Table, Button } from 'react-bootstrap';
 import { keccak256 as sha3 } from 'js-sha3';
 import { Link } from 'react-router-dom';
+import { multilanguage } from 'redux-multilanguage';
 
-export const MyCryptoModal = props => (
-  <Modal size='lg' show={props.showMyCrypto} onHide={props.changeShowMyCrypto}>
-    <Modal.Header closeButton>
-      <h3>Search <code>{props.name}</code> status on MyCrypto</h3>
-    </Modal.Header>
-    <Modal.Body>
-      <ol>
-        <li>Go to My Crypto contract interaction on your <a target='_blank' href='https://mycrypto.com/contracts/interact' rel='noopener noreferrer' className='modal-link'>browser</a> or native app.</li>
-        <li>Select <b>RSK MainNet</b> network on the top right selector.</li>
-        <li>Select <b>RNS Registrar</b> contract on <i>Existing Contract</i> selector.</li>
-        <li>Access!</li>
-        <li>On <i>Read / Write Contract</i> select <b>state</b></li>
-        <li>
-            <div>
-              Copy and paste this hash on <i>node bytes32</i>
-            </div>
-            <code>0x{sha3(props.name.split('.')[0])}</code>
-        </li>
-        <li>Read!</li>
-      </ol>
-      <Table striped bordered>
-        <thead>
-          <tr>
-          <th>Result</th>
-          <th>State</th>
-          <th>Next action</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-          <td><strong>0</strong></td>
-          <td>Open</td>
-          <td><Link to={`/start?domain=${props.name}`}>Start auction</Link></td>
-          </tr>
-          <tr>
-          <td><strong>1</strong></td>
-          <td>Auction</td>
-          <td><Link to={`/bid?domain=${props.name}`}>Bid in the auction</Link></td>
-          </tr>
-          <tr>
-          <td><strong>2</strong></td>
-          <td>Owned</td>
-          <td><Link to={`/finalize?domain=${props.name}`}>Finalize the auciton</Link></td>
-          </tr>
-          <tr>
-          <td><strong>4</strong></td>
-          <td>Reveal</td>
-          <td><Link to={`/unseal?domain=${props.name}`}>Unseal your bid</Link></td>
-          </tr>
-        </tbody>
-      </Table>
-    </Modal.Body>
-  </Modal>
-);
+export const MyCryptoModal = multilanguage(props => {
+  const { strings, name } = props;
+
+  return (
+    <Modal size='lg' show={props.showMyCrypto} onHide={props.changeShowMyCrypto}>
+      <Modal.Header closeButton>
+        <h3>{strings.mycrypto_search_title}</h3>
+        <code>{name}</code>
+      </Modal.Header>
+      <Modal.Body>
+        <ol>
+          <li>{strings.mycrypto_select_network}</li>
+          <li>{strings.mycrypto_go_to_interact}</li>
+          <li>{strings.mycrypto_select_registrar}</li>
+          <li>{strings.mycrypto_access}</li>
+          <li>{strings.mycrypto_on_read_write_select} <b>state</b></li>
+          <li>
+              <div>
+                {strings.my_crypto_copy_paste_on} <i>node bytes32</i>
+              </div>
+              <code>0x{sha3(name.split('.')[0])}</code>
+          </li>
+          <li>{strings.mycrypto_read}</li>
+        </ol>
+        <Table striped bordered>
+          <thead>
+            <tr>
+            <th>{strings.read}</th>
+            <th>{strings.state}</th>
+            <th>{strings.next_action}</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+            <td><strong>0</strong></td>
+            <td>{strings.open}</td>
+            <td><Link to={`/start?domain=${props.name}`}>{strings.process_step_1}</Link></td>
+            </tr>
+            <tr>
+            <td><strong>1</strong></td>
+            <td>{strings.auction}</td>
+            <td><Link to={`/bid?domain=${props.name}`}>{strings.process_step_2}</Link></td>
+            </tr>
+            <tr>
+            <td><strong>2</strong></td>
+            <td>{strings.owned}</td>
+            <td><Link to={`/finalize?domain=${props.name}`}>{strings.process_step_4}</Link></td>
+            </tr>
+            <tr>
+            <td><strong>4</strong></td>
+            <td>{strings.reveal}</td>
+            <td><Link to={`/unseal?domain=${props.name}`}>{strings.process_step_3}</Link></td>
+            </tr>
+          </tbody>
+        </Table>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant='primary' onClick={() => window.open('https://mycrypto.com/contracts/interact', '_blank')}>{strings.go_to_mycrypto}</Button>
+      </Modal.Footer>
+    </Modal>
+  )
+});
