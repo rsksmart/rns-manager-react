96bc3ac097158974f77de18f738f23cd66e6cc06
Ilan Olkies <ilanolkies@outlook.com>
2019-05-03 22:30:10 -0300

    i18n on no wallet page

--- src/app/tabs/NoMetamaskTab.js
+++ src/app/tabs/NoMetamaskTab.js
@@ -4,6 +4,7 @@ import rskWallet from '../../assets/rsk_wallet.png';
 import { changeMyCryptoMetamask } from './user/operations';
 import { connect } from 'react-redux';
 import { push } from 'connected-react-router';
+import { multilanguage } from 'redux-multilanguage';
 
 const setMyCryptoButton = props => (
   <Button onClick={() => props.setMyCrypto(props.viewMyCrypto)}>{props.children}</Button>
@@ -18,37 +19,41 @@ const mapDispatchToProps = dispatch => ({
 
 const SetMyCryptoButton = connect(null, mapDispatchToProps)(setMyCryptoButton);
 
-export const NoMetamaskTab = () => (
-  <Row>
-    <Col>
-      <Container>
-        <Row>
-          <Col>
-            <Image src={rskWallet} alt='rsk_wallet' fluid style={{ height: 300 }} />
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <p>you need an RSK wallet to resolve, register and admin domains</p>
-            <Button onClick={() => window.open('https://metamask.io', '_blank')}>get Metamask</Button>
-            <span> or </span>
-            <SetMyCryptoButton viewMyCrypto={true}>use with MyCrypto</SetMyCryptoButton>
-          </Col>
-        </Row>
-        <hr />
-        <Row>
-          <Col>
-          <p>and connect it to RSK network</p>
-          <Button onClick={() => window.open('/setup', '_blank')}>connect</Button>
-          </Col>
-        </Row>
-        <hr />
-        <Row>
-          <Col>
-            <SetMyCryptoButton viewMyCrypto={false}>done</SetMyCryptoButton>
-          </Col>
-        </Row>
-      </Container>
-    </Col>
-  </Row>
-);
+export const NoMetamaskTab = multilanguage(props => {
+  const { strings } = props;
+
+  return (
+    <Row>
+      <Col>
+        <Container>
+          <Row>
+            <Col>
+              <Image src={rskWallet} alt='rsk_wallet' fluid style={{ height: 300 }} />
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <p>{strings.rsk_wallet_needed}</p>
+              <Button onClick={() => window.open('https://metamask.io', '_blank')}>{strings.get_metamask}</Button>
+              <span> or </span>
+              <SetMyCryptoButton viewMyCrypto={true}>{strings.use_with_mycrypto}</SetMyCryptoButton>
+            </Col>
+          </Row>
+          <hr />
+          <Row>
+            <Col>
+            <p>{strings.connect_metamask_to_rsk}</p>
+            <Button onClick={() => window.open('/setup', '_blank')}>{strings.connect}</Button>
+            </Col>
+          </Row>
+          <hr />
+          <Row>
+            <Col>
+              <SetMyCryptoButton viewMyCrypto={false}>{strings.done}</SetMyCryptoButton>
+            </Col>
+          </Row>
+        </Container>
+      </Col>
+    </Row>
+  );
+});
