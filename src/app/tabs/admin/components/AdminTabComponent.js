0eb5a8a87ce3ba73fadc2ceb51984f2348d11506
Ilan Olkies <ilanolkies@outlook.com>
2019-05-08 17:11:45 -0300

    Show owner and ttl as advanced options

--- src/app/tabs/admin/components/AdminTabComponent.js
+++ src/app/tabs/admin/components/AdminTabComponent.js
@@ -1,49 +1,72 @@
-import React from 'react';
-import { Container, Col, Row } from 'react-bootstrap';
+import React, { Component } from 'react';
+import { Container, Col, Row, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { OwnerContainer, ResolverContainer, TtlContainer, SubdomainsListContainer } from '../containers';
 import { multilanguage } from 'redux-multilanguage';
 import { AuthTabWrapper } from '../../../auth';
 import { publicResolver, multiChainResolver } from '../../../../config/contracts.json';
 
-export default multilanguage(props => {
-  const { strings, name, resolver } = props;
+class AdminTabComponent extends Component {
+  constructor (props) {
+    super(props);
 
-  return (
-    <AuthTabWrapper>
-      <Container>
-        <h2>{strings.admin} <code>{name}</code></h2>
-        <Row>
-          <Col>
-            <OwnerContainer />
-          </Col>
-        </Row>
-        <hr />
-        <Row>
-          <Col>
-            <ResolverContainer />
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            {resolver === publicResolver && <Link to='/publicResolver'>{strings.admin_resolution}</Link>}
-            {resolver === multiChainResolver && <Link to='/multiChainResolver'>{strings.admin_resolution}</Link>}
-          </Col>
-        </Row>
-        <hr />
-        <Row>
-          <Col>
-            <TtlContainer />
-          </Col>
-        </Row>
-        <hr />
-        <Row>
-          <Col>
-            <h3>{strings.subdomains}</h3>
-            <SubdomainsListContainer />
-          </Col>
-        </Row>
-      </Container>
-    </AuthTabWrapper>
-  )
-});
+    this.state = { showAdvanced: false };
+
+    this.changeShowAdvanced = this.changeShowAdvanced.bind(this);
+  }
+
+  changeShowAdvanced () {
+    this.setState(state => ({ showAdvanced: !state.showAdvanced }));
+  }
+
+  render () {
+    const { strings, name, resolver } = this.props;
+    const { showAdvanced } = this.state;
+
+    return (
+      <AuthTabWrapper>
+        <Container>
+          <h2>{strings.admin} <code>{name}</code></h2>
+          <Row>
+            <Col>
+              <ResolverContainer />
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              {resolver === publicResolver && <Link to='/publicResolver'>{strings.admin_resolution}</Link>}
+              {resolver === multiChainResolver && <Link to='/multiChainResolver'>{strings.admin_resolution}</Link>}
+            </Col>
+          </Row>
+          <hr />
+          <Row>
+            <Col>
+              <h3>{strings.subdomains}</h3>
+              <SubdomainsListContainer />
+            </Col>
+          </Row>
+          <hr />
+          <Button variant='link' onClick={this.changeShowAdvanced}>{showAdvanced ? 'simple -' : 'advanced +'}</Button>
+          {
+            showAdvanced &&
+            <React.Fragment>
+              <Row>
+                <Col>
+                  <OwnerContainer />
+                </Col>
+              </Row>
+              <hr />
+              <Row>
+                <Col>
+                  <TtlContainer />
+                </Col>
+              </Row>
+            </React.Fragment>
+          }
+        </Container>
+      </AuthTabWrapper>
+    );
+  }
+}
+
+export default multilanguage(AdminTabComponent);
