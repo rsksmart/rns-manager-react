8b04409e4bd3cfe627d10c917366e0fe24450a80
Meri Herrera <herrerameri@gmail.com>
2019-05-27 20:14:06 -0300

    Test startAuction - reducer initialState

---
+++ test/reducers/startAuction.test.js
@@ -0,0 +1,10 @@
+import reducer from '../../src/app/tabs/startAuction/reducer'
+import { REQUEST_START_AUCTION, RECEIVE_START_AUCTION } from '../../src/app/tabs/startAuction/types';
+
+var initialState = { loading: false };
+
+describe('startAuction reducer', () => {
+  it('should return the initial state', () => {
+    expect(reducer(undefined, {})).toEqual(initialState)
+  })
+})
