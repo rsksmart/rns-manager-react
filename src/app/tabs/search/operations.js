0be212b97bca25af3d81582ec0a9f4b77ec4bdd9
Ilan Olkies <ilanolkies@outlook.com>
2019-06-06 12:42:22 -0300

    Search using rsk public nodes

--- src/app/tabs/search/operations.js
+++ src/app/tabs/search/operations.js
@@ -2,74 +2,43 @@ import { requestDomainState, receiveDomainState } from './actions';
 import { keccak_256 as sha3 } from 'js-sha3';
 import { registrar as registrarAddress } from '../../../config/contracts';
 import { notifyError } from '../../notifications';
+import Web3 from 'web3';
+import { rskMain } from '../../../config/nodes';
+import { registrarAbi, deedAbi } from './abis';
 
 export const getAuctionState = domain => dispatch => {
   if (!domain) {
-    dispatch(receiveDomainState(''));
-    return;
+    return dispatch(receiveDomainState(''));
   }
 
   dispatch(requestDomainState());
 
-  const registrar = window.web3 && window.web3.eth.contract([
-    {
-      'constant': true,
-      'inputs': [
-        { 'name': '_hash', 'type': 'bytes32' }
-      ],
-      'name': 'entries',
-      'outputs': [
-        { 'name': '', 'type': 'uint8' },
-        { 'name': '', 'type': 'address' },
-        { 'name': '', 'type': 'uint256' },
-        { 'name': '', 'type': 'uint256' },
-        { 'name': '', 'type': 'uint256' }
-      ],
-      'payable': false,
-      'stateMutability': 'view',
-      'type': 'function'
-    }
-  ]).at(registrarAddress);
+  const web3 = new Web3(rskMain);
+
+  const registrar = new web3.eth.Contract(registrarAbi, registrarAddress);
 
   const hash = '0x' + sha3(domain.split('.')[0]);
 
-  return new Promise(resolve => {
-    registrar.entries(hash, (error, result) => {
-      if (error) return resolve(dispatch(notifyError(error.message)));
+  registrar.methods.entries(hash).call()
+  .then(entry => {
+    let state = entry[0];
 
-      let state = result[0].toNumber();
+    if (state !== 2) {
+      return dispatch(receiveDomainState(state))
+    }
 
-      if (state === 2) {
-        const deedAddress = result[1];
+    const deedAddress = entry[1];
 
-        const deed = window.web3.eth.contract([
-          {
-            'constant': true,
-            'inputs': [],
-            'name': 'expirationDate',
-            'outputs': [
-              {
-                'name': '',
-                'type': 'uint256'
-              }
-            ],
-            'payable': false,
-            'stateMutability': 'view',
-            'type': 'function'
-          }
-        ]).at(deedAddress);
+    const deed = new web3.eth.Contract(deedAbi, deedAddress);
 
-        return deed.expirationDate((errorDeed, resultDeed) => {
-          if (errorDeed) return resolve(dispatch(notifyError(errorDeed.message)));
-
-          const expirationDate = resultDeed.toNumber();
-          const status = expirationDate === 0 ? 2 : 5;
-
-          return resolve(dispatch(receiveDomainState(status)));
-        });
+    deed.methods.expirationDate().call()
+    .then(expirationDate => {
+      if (expirationDate === 0) {
+        return dispatch(receiveDomainState(2));
       }
 
-      return resolve(dispatch(receiveDomainState(state)));
+      return dispatch(receiveDomainState(5));
     });
-  });
+  })
+  .catch(error => dispatch(notifyError(error.message)))
 };
