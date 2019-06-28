881050587238f6ed8c6069651aeb15a20e1f8724
Ilan Olkies <ilanolkies@outlook.com>
2019-06-28 15:22:18 -0300

    eslint src/app/tabs/user/actions.js

--- src/app/tabs/user/actions.js
+++ src/app/tabs/user/actions.js
@@ -1,6 +1,7 @@
-import { CHANGE_MYCRYPTO_METAMASK } from "./types";
+import { CHANGE_MYCRYPTO_METAMASK } from './types';
 
+// eslint-disable-next-line import/prefer-default-export
 export const changeMyCryptoMetamask = viewMyCrypto => ({
   type: CHANGE_MYCRYPTO_METAMASK,
-  viewMyCrypto
+  viewMyCrypto,
 });
