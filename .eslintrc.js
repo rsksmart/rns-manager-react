0f5b4ecabeaef859d03172f5c03c5f7f164b0045
Ilan Olkies <ilanolkies@outlook.com>
2019-06-28 14:45:23 -0300

    eslint src/app/tabs/user/reducer.test.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
+    "jest/globals": true
   },
   extends: [
     'airbnb',
@@ -19,6 +20,7 @@ module.exports = {
   },
   plugins: [
     'react',
+    'jest'
   ],
   rules: {
     "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }]
