0384e2a7b87ae6cae250da486a794b4506a37ba2
Ilan Olkies <ilanolkies@outlook.com>
2019-06-28 15:19:16 -0300

    eslint src/app/tabs/user/components/

--- src/app/tabs/user/components/LanguageSelectComponent.js
+++ src/app/tabs/user/components/LanguageSelectComponent.js
@@ -1,38 +1,51 @@
 import React, { Component } from 'react';
-import { Row, Col, FormGroup, Form } from 'react-bootstrap'
+import propTypes from 'prop-types';
+import {
+  Row, Col, FormGroup, Form,
+} from 'react-bootstrap';
 import { multilanguage } from 'redux-multilanguage';
 
 class LanguageSelectComponent extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
 
-    this.state ={ language: props.language };
+    this.state = { language: props.language };
 
     this.onLanguageChange = this.onLanguageChange.bind(this);
   }
 
-  onLanguageChange (event) {
+  onLanguageChange(event) {
     const { value } = event.target;
     const { changeLanguage } = this.props;
+
     this.setState({ language: value });
+
     changeLanguage(value);
   }
 
-  render () {
+  render() {
     const { strings } = this.props;
+    const { language } = this.state;
 
     return (
       <FormGroup as={Row}>
         <Form.Label column md={4}>{strings.language}</Form.Label>
         <Col md={8}>
-          <Form.Control as='select' onChange={this.onLanguageChange} value={this.state.language}>
-            <option value='en'>English</option>
+          <Form.Control as="select" onChange={this.onLanguageChange} value={language}>
+            <option value="en">English</option>
           </Form.Control>
         </Col>
       </FormGroup>
-    )
+    );
   }
 }
 
-export default multilanguage(LanguageSelectComponent);
+LanguageSelectComponent.propTypes = {
+  language: propTypes.string.isRequired,
+  changeLanguage: propTypes.func.isRequired,
+  strings: propTypes.shape({
+    language: propTypes.string,
+  }).isRequired,
+};
 
+export default multilanguage(LanguageSelectComponent);
