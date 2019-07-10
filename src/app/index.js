4960befb6106d17eca37f3c0267641d1faa3a1f9
Ilan Olkies <ilanolkies@outlook.com>
2019-07-10 17:05:31 -0300

    eslint src/app/

--- src/app/index.js
+++ src/app/index.js
@@ -1,29 +1,34 @@
 import React, { Component } from 'react';
-import { Container, Row, Col, Alert } from 'react-bootstrap';
+import propTypes from 'prop-types';
+import {
+  Container, Row, Col, Alert,
+} from 'react-bootstrap';
 import { ConnectedRouter } from 'connected-react-router';
+import { loadLanguages, multilanguage } from 'redux-multilanguage';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import Routes from './routes';
 import { HeaderContainer } from './containers';
 import { AuthModal } from './auth';
-import { Notifications } from './notifications';
-import { loadLanguages } from 'redux-multilanguage'
-import { connect } from 'react-redux';
-import { notificationTypes } from './notifications';
-import { multilanguage } from 'redux-multilanguage';
-import { Link } from 'react-router-dom';
+import { Notifications, notificationTypes } from './notifications';
+
+
 import { multiChainResolver } from '../config/contracts';
 
 class App extends Component {
-  componentDidMount () {
+  componentDidMount() {
     const { dispatch } = this.props;
 
     dispatch(loadLanguages({
       languages: {
-        en: require('../languages/en.json')
-      }
+        // eslint-disable-next-line global-require
+        en: require('../languages/en.json'),
+      },
     }));
   }
 
   render() {
+    // eslint-disable-next-line react/prop-types
     const { strings, history, multiChainNotification } = this.props;
 
     return (
@@ -32,13 +37,15 @@ class App extends Component {
           <HeaderContainer />
           <Container style={{ marginTop: '20px' }}>
             {
-              multiChainNotification &&
-              <Alert variant='info' dismissible onClose={() => localStorage.setItem('multichain_resolver_dissimised', 1)}>
+              multiChainNotification
+              && (
+              <Alert variant="info" dismissible onClose={() => localStorage.setItem('multichain_resolver_dissimised', 1)}>
                 <Alert.Heading>{strings.migrate_multichain_resolver_title}</Alert.Heading>
                 {strings.migrate_multichain_resolver_message}
                 <hr />
-                <Link to={`/admin?action=resolver&defaultValue=${multiChainResolver}`} className='btn btn-primary'>migrate</Link>
+                <Link to={`/admin?action=resolver&defaultValue=${multiChainResolver}`} className="btn btn-primary">migrate</Link>
               </Alert>
+              )
             }
             <Row>
               <Col>
@@ -64,8 +71,17 @@ class App extends Component {
   }
 }
 
+App.propTypes = {
+  dispatch: propTypes.func.isRequired,
+  strings: propTypes.shape({
+    migrate_multichain_resolver_title: propTypes.string.isRequired,
+    migrate_multichain_resolver_message: propTypes.string.isRequired,
+  }).isRequired,
+  multiChainNotification: propTypes.bool.isRequired,
+};
+
 const mapStateToProps = state => ({
-  multiChainNotification: state.notifications.find(n => n.type === notificationTypes.MIGRATE_RESOLVER) && !localStorage.getItem('multichain_resolver_dissimised')
+  multiChainNotification: state.notifications.find(n => n.type === notificationTypes.MIGRATE_RESOLVER) && !localStorage.getItem('multichain_resolver_dissimised'),
 });
 
 export default connect(mapStateToProps)(multilanguage(App));
--- src/app/reducers.js
+++ src/app/reducers.js
@@ -1,18 +1,18 @@
 import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router';
+import { createMultilanguageReducer } from 'redux-multilanguage';
 import tabReducers from './tabs';
 import authReducer from './auth';
 import notificationReducer from './notifications';
-import { createMultilanguageReducer } from 'redux-multilanguage';
 
 const language = localStorage.getItem('language') || 'en';
 
-const rootReducer = (history) => combineReducers({
+const rootReducer = history => combineReducers({
   ...tabReducers,
   auth: authReducer,
   notifications: notificationReducer,
   router: connectRouter(history),
-  multilanguage: createMultilanguageReducer({ currentLanguageCode: language })
+  multilanguage: createMultilanguageReducer({ currentLanguageCode: language }),
 });
 
 export default rootReducer;
