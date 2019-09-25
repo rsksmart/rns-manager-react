7405491b91c31de1c6e9ec92d7421e6f4e79c27d
Javi Esses <javiesses@hotmail.com>
2019-09-25 11:32:30 -0300

    Remove unused line

--- src/app/types.js
+++ src/app/types.js
@@ -2,4 +2,3 @@ export const ADDRESS = 'ADDRESS';
 export const POSITIVE_NUMBER = 'POSITIVE_NUMBER';
 export const BYTES32 = 'BYTES32';
 export const RESOLVER = 'RESOLVER';
-export const STRING = 'STRING';
