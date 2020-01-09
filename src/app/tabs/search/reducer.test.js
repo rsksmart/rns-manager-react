f7e97094de9e6cbe718075e9c53c3f5b1ee80418
Jesse Clark <hello@developerjesse.com>
2020-01-09 19:26:16 +0200

    Fix failing tests amd add test for REQUEST_DOMAIN_COST.

--- src/app/tabs/search/reducer.test.js
+++ src/app/tabs/search/reducer.test.js
@@ -1,7 +1,7 @@
 import reducer from './reducer';
 import {
   REQUEST_DOMAIN_STATE, RECEIVE_DOMAIN_STATE, BLOCKED_DOMAIN,
-  REQUEST_DOMAIN_OWNER, RECEIVE_DOMAIN_OWNER, RECEIVE_DOMAIN_COST,
+  REQUEST_DOMAIN_OWNER, RECEIVE_DOMAIN_OWNER, RECEIVE_DOMAIN_COST, REQUEST_DOMAIN_COST,
 } from './types';
 
 describe('search reducer', () => {
@@ -14,6 +14,7 @@ describe('search reducer', () => {
           domainStateLoading: false,
           blocked: undefined,
           requestingOwner: false,
+          requestingCost: false,
           rifCost: 0,
         },
       );
@@ -218,21 +219,30 @@ describe('search reducer', () => {
           domainStateLoading: false,
           blocked: undefined,
           requestingOwner: false,
+          requestingCost: false,
           rifCost: 0,
         },
       );
   });
 
-  it('should handle RECEIVE_DOMAIN_COST', () => {
+  it('should handle REQUEST_DOMAIN_COST and RECEIVE_DOMAIN_COST', () => {
+    expect(
+      reducer({}, {
+        type: REQUEST_DOMAIN_COST,
+      }),
+    ).toEqual({
+      requestingCost: true,
+    });
+
     expect(
       reducer({
         rifcost: 2,
       }, {
         type: RECEIVE_DOMAIN_COST,
       }),
-    )
-      .toEqual({
-        rifcost: 2,
-      });
+    ).toEqual({
+      rifcost: 2,
+      requestingCost: false,
+    });
   });
 });
