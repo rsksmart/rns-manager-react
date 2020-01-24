0383ba5d7a93174b1f7a09ca051443cdf453ce06
Jesse Clark <hello@developerjesse.com>
2020-01-24 19:33:33 +0200

    Create error page with english in a single file.

--- src/app/tabs/error/index.js
+++ src/app/tabs/error/index.js
@@ -1,10 +1,64 @@
 import React from 'react';
+import propTypes from 'prop-types';
+import { connect } from 'react-redux';
+import rskWallet from '../../../assets/rsk_wallet.png';
 
-const ErrorTab = () => {
-  console.log('MyErrorTab');
+import { networkSelector } from '../../selectors';
+
+const DetailsSectionComponent = ({
+  hasMetamask, walletNetwork, envNetwork,
+}) => {
+  if (!hasMetamask) {
+    return (
+      <div>
+        <img src={rskWallet} alt="rsk_wallet" width="250px" />
+        <h2>No metamask detected</h2>
+      </div>
+    );
+  }
+
   return (
-    <h1>Error Tab yo!</h1>
+    <div>
+      <img src={rskWallet} alt="rsk_wallet" width="250px" />
+      <h2>Network Mismatch</h2>
+      <p>
+        Your wallet is connected to
+        <strong>
+          {' '}
+          {walletNetwork}
+        </strong>
+        .
+        <br />
+        Your wallet should be connected to
+        <strong>
+          {' '}
+          {envNetwork}
+        </strong>
+        .
+      </p>
+    </div>
   );
 };
+
+DetailsSectionComponent.defaultProps = {
+  walletNetwork: '',
+};
+
+DetailsSectionComponent.propTypes = {
+  hasMetamask: propTypes.bool.isRequired,
+  walletNetwork: propTypes.string,
+  envNetwork: propTypes.string.isRequired,
+};
+
+const mapStateToProps = state => ({
+  hasMetamask: state.auth.hasMetamask,
+  networkMatch: state.auth.networkMatch,
+  walletNetwork: networkSelector(state.auth.network),
+  envNetwork: networkSelector(process.env.REACT_APP_ENVIRONMENT_ID),
+});
+
+const DetailsSection = connect(mapStateToProps, null)(DetailsSectionComponent);
+
+const ErrorTab = () => (<DetailsSection />);
 
 export default ErrorTab;
