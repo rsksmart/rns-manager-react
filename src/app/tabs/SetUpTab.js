f1e93cc9138a077c5eedf2c5c15ebb57bac9fc22
Jesse Clark <hello@developerjesse.com>
2020-02-17 17:24:05 +0200

    Add return() statement in component
    - No additional code was changed
    - Allows variable and code to be added above in pending commits.

--- src/app/tabs/SetUpTab.js
+++ src/app/tabs/SetUpTab.js
@@ -5,56 +5,59 @@ import { multilanguage } from 'redux-multilanguage';
 import { StartButton } from '../auth';
 import { explorer } from '../adapters/explorerAdapter';
 
-const SetUpTab = multilanguage(({ strings }) => (
-  <Container>
-    <Row>
-      <Col style={{ textAlign: 'left' }}>
-        <h2>{strings.setup_title}</h2>
-        <ol>
-          <li>{strings.setup_download_metamask}</li>
-          <li>
-            {strings.setup_connect_to_rsk}
-            <ol>
-              <li>{strings.setup_open_network_selector}</li>
-              <li>{strings.setup_select_custom_rpc}</li>
-              <li>
-                {strings.setup_fill_with}
-                <ul>
-                  <li>Network Name: RSK</li>
-                  <li>New RPC URL: https://public-node.rsk.co</li>
-                  <li>ChainID: 30</li>
-                  <li>Symbol: RBTC</li>
-                  <li>Nickname: RSK MainNet</li>
-                  <li>{`Block Explorer URL: ${explorer}`}</li>
-                </ul>
-              </li>
-              <li>{strings.setup_save}</li>
-            </ol>
-          </li>
-          <li>
-            {strings.setup_add_rif}
-            <ol>
-              <li>{strings.setup_open_left_menu}</li>
-              <li>{strings.setup_go_to_add_token}</li>
-              <li>
-                {/* eslint-disable-next-line no-underscore-dangle */}
-                {strings.setup_fill_token_address_with_}
-                {' 0x2acc95758f8b5f583470ba265eb685a8f45fc9d5'}
-              </li>
-              <li>{strings.setup_next}</li>
-              <li>{strings.setup_add_tokens}</li>
-            </ol>
-          </li>
-        </ol>
-      </Col>
-    </Row>
-    <Row>
-      <Col>
-        <StartButton />
-      </Col>
-    </Row>
-  </Container>
-));
+const SetUpTab = multilanguage(({ strings }) => {
+
+  return (
+    <Container>
+      <Row>
+        <Col style={{ textAlign: 'left' }}>
+          <h2>{strings.setup_title}</h2>
+          <ol>
+            <li>{strings.setup_download_metamask}</li>
+            <li>
+              {strings.setup_connect_to_rsk}
+              <ol>
+                <li>{strings.setup_open_network_selector}</li>
+                <li>{strings.setup_select_custom_rpc}</li>
+                <li>
+                  {strings.setup_fill_with}
+                  <ul>
+                    <li>Network Name: RSK</li>
+                    <li>New RPC URL: https://public-node.rsk.co</li>
+                    <li>ChainID: 30</li>
+                    <li>Symbol: RBTC</li>
+                    <li>Nickname: RSK MainNet</li>
+                    <li>{`Block Explorer URL: ${explorer}`}</li>
+                  </ul>
+                </li>
+                <li>{strings.setup_save}</li>
+              </ol>
+            </li>
+            <li>
+              {strings.setup_add_rif}
+              <ol>
+                <li>{strings.setup_open_left_menu}</li>
+                <li>{strings.setup_go_to_add_token}</li>
+                <li>
+                  {/* eslint-disable-next-line no-underscore-dangle */}
+                  {strings.setup_fill_token_address_with_}
+                  {' 0x2acc95758f8b5f583470ba265eb685a8f45fc9d5'}
+                </li>
+                <li>{strings.setup_next}</li>
+                <li>{strings.setup_add_tokens}</li>
+              </ol>
+            </li>
+          </ol>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <StartButton />
+        </Col>
+      </Row>
+    </Container>
+  );
+});
 
 SetUpTab.propTypes = {
   strings: propTypes.arrayOf(propTypes.string).isRequired,
