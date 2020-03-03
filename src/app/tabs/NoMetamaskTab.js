694e1685a6c5cbc523a9f778a651eb72ebb935e7
Jesse Clark <hello@developerjesse.com>
2020-03-03 12:37:57 +0200

    Remove myCrypto button.

--- src/app/tabs/NoMetamaskTab.js
+++ src/app/tabs/NoMetamaskTab.js
@@ -3,23 +3,9 @@ import propTypes from 'prop-types';
 import {
   Container, Row, Col, Image, Button,
 } from 'react-bootstrap';
-import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { multilanguage } from 'redux-multilanguage';
 import rskWallet from '../../assets/rsk_wallet.png';
-
-const setMyCryptoButton = ({ setMyCrypto, viewMyCrypto, children }) => (
-  <Button onClick={() => setMyCrypto(viewMyCrypto)}>{children}</Button>
-);
-
-setMyCryptoButton.propTypes = {
-  setMyCrypto: propTypes.func.isRequired,
-  viewMyCrypto: propTypes.func.isRequired,
-  children: propTypes.node.isRequired,
-};
-
-const mapDispatchToProps = () => ({});
-
-const SetMyCryptoButton = connect(null, mapDispatchToProps)(setMyCryptoButton);
 
 const NoMetamaskTab = multilanguage(({ strings }) => (
   <Row>
@@ -40,13 +26,7 @@ const NoMetamaskTab = multilanguage(({ strings }) => (
         <Row>
           <Col>
             <p>{strings.connect_metamask_to_rsk}</p>
-            <Button onClick={() => window.open('/setup', '_blank')}>{strings.connect}</Button>
-          </Col>
-        </Row>
-        <hr />
-        <Row>
-          <Col>
-            <SetMyCryptoButton viewMyCrypto={false}>{strings.done}</SetMyCryptoButton>
+            <Link to="/setup" className="btn btn-primary">{strings.connect}</Link>
           </Col>
         </Row>
       </Container>
