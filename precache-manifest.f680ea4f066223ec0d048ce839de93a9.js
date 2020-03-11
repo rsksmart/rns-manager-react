da7a86eca040f08d12031c1830095ac18b171a75
Ilan Olkies <ilanolkies@outlook.com>
2020-03-11 18:13:08 -0300

    Updates

---
+++ precache-manifest.f680ea4f066223ec0d048ce839de93a9.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "116c3a7c1ac2e94a84b7",
+    "url": "/rns-manager-react/static/css/main.2abbd6f5.chunk.css"
+  },
+  {
+    "revision": "116c3a7c1ac2e94a84b7",
+    "url": "/rns-manager-react/static/js/main.116c3a7c.chunk.js"
+  },
+  {
+    "revision": "4393c63a30a01e8ac420",
+    "url": "/rns-manager-react/static/js/runtime~main.4393c63a.js"
+  },
+  {
+    "revision": "0dd90ce815a7dc0ae4ec",
+    "url": "/rns-manager-react/static/js/2.0dd90ce8.chunk.js"
+  },
+  {
+    "revision": "200a93b5f36c556939a8feeb34ab2e14",
+    "url": "/rns-manager-react/static/media/rsk_wallet.200a93b5.png"
+  },
+  {
+    "revision": "2d03633a4a95192a0ecb1310331ca125",
+    "url": "/rns-manager-react/static/media/close-blue.2d03633a.svg"
+  },
+  {
+    "revision": "3a8853eddde3c555cd8261dd50007d5a",
+    "url": "/rns-manager-react/static/media/logo.3a8853ed.svg"
+  },
+  {
+    "revision": "9ccedb653152070f28df94e345d19872",
+    "url": "/rns-manager-react/static/media/logo-testnet.9ccedb65.svg"
+  },
+  {
+    "revision": "a6953932909bddeb2acbbea3867107f3",
+    "url": "/rns-manager-react/static/media/powered_by_iov.a6953932.svg"
+  },
+  {
+    "revision": "2f491c35f76e47eb7194ddfcb7691775",
+    "url": "/rns-manager-react/static/media/close-red.2f491c35.svg"
+  },
+  {
+    "revision": "0c024c39f36b48c93eaef7a93c249cd0",
+    "url": "/rns-manager-react/static/media/check-blue.0c024c39.svg"
+  },
+  {
+    "revision": "c5b877ca050b26e8cc58deab0c47f499",
+    "url": "/rns-manager-react/static/media/edit.c5b877ca.svg"
+  },
+  {
+    "revision": "cad75a4f8d5a704e86e254addd921347",
+    "url": "/rns-manager-react/static/media/edit-active.cad75a4f.svg"
+  },
+  {
+    "revision": "118758dde2b99649c1a3e1e72500ab2c",
+    "url": "/rns-manager-react/static/media/copy.118758dd.svg"
+  },
+  {
+    "revision": "1cd671f2faf8fbcd87bffb3b857e8c03",
+    "url": "/rns-manager-react/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/rns-manager-react/precache-manifest.40317100863447c781c991ae85777998.js"
+  "/rns-manager-react/precache-manifest.f680ea4f066223ec0d048ce839de93a9.js"
 );
 
 workbox.clientsClaim();
