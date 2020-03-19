39ea6ad35069876cbdb93cffb6be8e4ed055c0da
Jesse Clark <hello@developerjesse.com>
2020-03-19 14:39:44 +0200

    Move Addresses into its own folder.

--- src/app/tabs/newAdmin/addresses/containers/AddressesContainer.js
+++ src/app/tabs/newAdmin/addresses/containers/AddressesContainer.js
@@ -1,10 +1,8 @@
 import { connect } from 'react-redux';
 import { AddressesComponent } from '../components';
 
-// eslint-disable-next-line no-unused-vars
 const mapStateToProps = state => ({});
 
-// eslint-disable-next-line no-unused-vars
 const mapDispatchToProps = dispatch => ({});
 
 export default connect(
--- src/app/tabs/newAdmin/components/index.js
+++ src/app/tabs/newAdmin/components/index.js
@@ -1,4 +1,3 @@
 export { default as AdminTabComponent } from './AdminTabComponent';
-export { default as AddressesComponent } from './AddressesComponent';
 export { default as ResolverComponent } from './ResolverComponent';
 export { default as LeftNavComponent } from './LetftNavComponent';
