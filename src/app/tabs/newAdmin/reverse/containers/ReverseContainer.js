c829ac6fc39583f2214a2ad6142d0e93473ab936
Jesse Clark <hello@developerjesse.com>
2020-04-10 14:54:47 +0300

    Show entire label for Reverse with correct checksum

--- src/app/tabs/newAdmin/reverse/containers/ReverseContainer.js
+++ src/app/tabs/newAdmin/reverse/containers/ReverseContainer.js
@@ -4,6 +4,7 @@ import { ReverseComponent } from '../components';
 const mapStateToProps = state => ({
   reverseValue: state.newAdmin.reverse.value,
   address: state.auth.address,
+  chainId: state.auth.network,
   isRequesting: state.newAdmin.reverse.isRequesting,
   isWaiting: state.newAdmin.reverse.isWaiting,
   isError: state.newAdmin.reverse.isError,
