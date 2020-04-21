3e90115e2779f1e7c30bb74eb7263b9b9edfa575
Jesse Clark <hello@developerjesse.com>
2020-04-21 15:02:36 +0300

    Shuffle messages in container.

--- src/app/tabs/registrar/containers/KeyMessagesContainer.js
+++ src/app/tabs/registrar/containers/KeyMessagesContainer.js
@@ -2,9 +2,24 @@ import { connect } from 'react-redux';
 import TextRotationComponent from '../../../components/TextRotationComponent';
 import keyMessages from '../../../../languages/key_messges.json';
 
+const shuffle = (array) => {
+  let counter = array.length;
+  const returnAray = [];
+
+  while (counter > 0) {
+    const index = Math.floor(Math.random() * counter);
+    counter -= 1;
+
+    const temp = array[counter];
+    returnAray[counter] = array[index];
+    returnAray[index] = temp;
+  }
+
+  return returnAray;
+};
+
 const mapStateToProps = state => ({
-  heading: 'Did you know?',
-  messages: keyMessages,
+  messages: shuffle(keyMessages),
   language: state.multilanguage.currentLanguageCode,
 });
 
