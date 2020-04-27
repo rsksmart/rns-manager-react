95c7b44be7df25ca5ab330a48ddda00fdbafd45c
Jesse Clark <hello@developerjesse.com>
2020-04-27 15:50:22 +0300

    Update package.json to show console.logs and add testing environment variables.

---
+++ tests/setEnvVars.js
@@ -0,0 +1,5 @@
+process.env.REACT_APP_ENVIRONMENT = 'test';
+process.env.REACT_APP_ENVIRONMENT_ID = 31;
+process.env.REACT_APP_NODE = 'https://public-node.testnet.rsk.co';
+process.env.REACT_APP_GAS_PRICE = 60000000;
+process.env.REACT_APP_BLOCK_EXPLORER = 'https://explorer.testnet.rsk.co';
