93b8ff2d5a570f05bbadcd6c9a2e629a3d10acb3
Jesse Clark <hello@developerjesse.com>
2020-04-29 13:56:59 +0300

    write tests to interact with the ganache-cli.

--- tests/setupEnv.test.js
+++ tests/setupEnv.test.js
@@ -1,7 +1,18 @@
+/* eslint-disable arrow-body-style */
+import Web3 from 'web3';
+import ganache from 'ganache-cli';
+
+const ONE_HUNDREAD = 100000000000000000000;
+const ONE = ONE_HUNDREAD / 100;
+
 describe('set up RSK environment, contracts, etc', () => {
-  beforeAll(() => {
-    console.log('before all');
-    // start RSK network
+  let web3;
+  let accounts;
+  beforeAll(async () => {
+    // setup blockchain,
+    // const ganache = require('ganache-cli');
+    web3 = new Web3(ganache.provider());
+    accounts = await web3.eth.getAccounts();
   });
 
   beforeEach(() => {
@@ -16,5 +27,31 @@ describe('set up RSK environment, contracts, etc', () => {
   it('returns true', () => {
     console.log('test run');
     expect(true).toBeTruthy();
+  });
+
+  it('gets the block number', async () => {
+    const currentBlock = await web3.eth.getBlock('latest');
+    expect(currentBlock.number).toBe(0);
+  });
+
+  it('first account exists and has a balance', async () => {
+    expect(accounts[0]).toBeTruthy();
+
+    const balance = await web3.eth.getBalance(accounts[0]);
+    expect(parseInt(balance, 10)).toBe(ONE_HUNDREAD);
+  });
+
+  it('can send a transaction from one account to another', async () => {
+    return web3.eth.sendTransaction({
+      from: accounts[0],
+      to: accounts[1],
+      value: ONE,
+    })
+      .then(async () => {
+        const newBalance1 = await web3.eth.getBalance(accounts[0]);
+        const newBalance2 = await web3.eth.getBalance(accounts[1]);
+        expect(newBalance1 < ONE_HUNDREAD).toBeTruthy();
+        expect(newBalance2 > ONE_HUNDREAD).toBeTruthy();
+      });
   });
 });
