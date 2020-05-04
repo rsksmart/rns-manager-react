3346ed2cf30412e3a87e9691497b549fc4e91838
Jesse Clark <hello@developerjesse.com>
2020-05-04 14:36:51 +0300

    Add logic to determin blockchain and write addresses to config file.

--- tests/setEnvVars.js
+++ tests/setEnvVars.js
@@ -1,4 +1,4 @@
-process.env.REACT_APP_ENVIRONMENT = 'test';
+process.env.REACT_APP_ENVIRONMENT = 'circleci';
 process.env.REACT_APP_ENVIRONMENT_ID = 31;
 process.env.REACT_APP_NODE = 'https://public-node.testnet.rsk.co';
 process.env.REACT_APP_GAS_PRICE = 60000000;
--- tests/setupRnsSuite.js
+++ tests/setupRnsSuite.js
@@ -2,11 +2,10 @@ const RNSSuite = require('@rsksmart/rns-suite');
 const fs = require('fs');
 
 // set the blockchainURL. Use 0.0.0.0:8545 for CircleCi
-// const blockchainUrl = 'http://127.0.0.1:7545';
-const blockchainUrl = 'http://0.0.0.0:8545';
+const blockchainUrl = process.env.CIRCLECI ? 'http://0.0.0.0:8545' : 'http://127.0.0.1:8545';
 
 // eslint-disable-next-line no-console
-console.log(`Setting up RNS suite on ${blockchainUrl}`);
+console.log(`Setting up RNS suite on ${blockchainUrl} : ${process.env.CIRCLECI}`);
 
 RNSSuite(
   blockchainUrl,
@@ -29,7 +28,7 @@ RNSSuite(
   // write a contracts to a local file to be called in a bit:
   const stream = fs.createWriteStream('./src/config/contracts.circleci.json');
   stream.once('open', () => {
-    stream.write(JSON.stringify(contracts));
+    stream.write(JSON.stringify(contracts).toLowerCase());
     stream.end();
   });
 });
