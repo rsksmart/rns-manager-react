41c2c7d5b79c83d5cb835752b35e055aba032881
Jesse Clark <hello@developerjesse.com>
2020-05-05 16:06:08 +0300

    Expand Error Component to take constants produced by operations files
    - This allows translations for common errors with the ability to expand it later.

--- src/app/components/UserErrorComponent.test.js
+++ src/app/components/UserErrorComponent.test.js
@@ -8,6 +8,8 @@ import en from '../../languages/en.json';
 
 const store = mockStore({
   close: en.close,
+  same_value: en.same_value,
+  resolve_not_set: en.resolve_not_set,
 });
 
 describe('UserErrorComponent', () => {
@@ -35,5 +37,25 @@ describe('UserErrorComponent', () => {
     );
 
     expect(component.html()).toEqual('');
+  });
+
+  it('returns standard message when sent a constant for no resolver name', () => {
+    const component = mount(
+      <Provider store={store}>
+        <UserErrorComponent message="ERROR_RESOLVE_NAME" visible />
+      </Provider>,
+    );
+
+    expect(component.find('p').at(1).text()).toEqual(en.resolve_not_set);
+  });
+
+  it('returns standard message when sent a constant for same value', () => {
+    const component = mount(
+      <Provider store={store}>
+        <UserErrorComponent message="ERROR_SAME_VALUE" visible />
+      </Provider>,
+    );
+
+    expect(component.find('p').at(1).text()).toEqual(en.same_value);
   });
 });
