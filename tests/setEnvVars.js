989437da803113fd6acbea16e8bbf870a3d4af42
Jesse Clark <hello@developerjesse.com>
2020-05-22 14:05:43 +0300

    Add share card.

--- tests/setEnvVars.js
+++ tests/setEnvVars.js
@@ -3,3 +3,4 @@ process.env.REACT_APP_ENVIRONMENT_ID = 8545;
 process.env.REACT_APP_NODE = 'http://127.0.0.1:8545';
 process.env.REACT_APP_GAS_PRICE = 60000000;
 process.env.REACT_APP_BLOCK_EXPLORER = 'https://explorer.testnet.rsk.co';
+process.env.REACT_APP_URL = '/';
