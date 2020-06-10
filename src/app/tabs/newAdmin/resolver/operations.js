ac6d3f8a0bfde3e4e1709bec08004ee400851b2d
Jesse Clark <hello@developerjesse.com>
2020-06-10 14:30:54 +0300

    Remove comments and update tests.

--- src/app/tabs/newAdmin/resolver/operations.js
+++ src/app/tabs/newAdmin/resolver/operations.js
@@ -268,7 +268,6 @@ const setContractAbi = (resolverAddress, domain, value) => async (dispatch) => {
   let dataSourceError;
   let parsedJson;
   const response = [];
-  console.log('setting Contract Abi!', resolverAddress, domain, value);
 
   // get data by input method starting with URI:
   if (value.inputMethod === 'uri') {
@@ -301,59 +300,44 @@ const setContractAbi = (resolverAddress, domain, value) => async (dispatch) => {
 
   // type 1: uncompressed Json
   if (value.encodings.json && parsedJson !== '') {
-    console.log('adding 1: JSON', parsedJson);
     response.push({ id: 1, result: web3.utils.toHex(parsedJson) });
   } else if (value.isEditing && !value.encodings.json) {
-    // if editing, and uri is not checked, erase the value just in case
-    console.log('blank 1: JSON');
     response.push({ id: 1, result: 0 });
   }
 
-  // type 2:
+  // type 2: zlib compression
   if (value.encodings.zlib && parsedJson !== '') {
-    console.log('adding 2: zLIB');
     const zLibPromise = new Promise((resolve, reject) => {
       deflate(parsedJson, async (err, buffer) => {
-        console.log('deflateFinished');
         if (err) {
           return reject(dispatch(errorSetContent(CONTRACT_ABI, 'Error creating zlib package')));
         }
         const zlibCompressed = web3.utils.toHex(buffer);
-        console.log('zlib to hex', zlibCompressed);
         return resolve(zlibCompressed);
       });
     });
 
     await zLibPromise.then(zlibHex => response.push({ id: 2, result: zlibHex }));
   } else if (value.isEditing && !value.encodings.zlib) {
-    console.log('blank 2: URI');
     response.push({ id: 2, result: 0 });
   }
 
-  // type 4:
+  // type 4: cbor compression
   if (value.encodings.cbor && parsedJson !== '') {
-    console.log('adding 4: cbor');
-    const cborEncoded = web3.utils.toHex(cbor.encode(parsedJson));
     response.push({ id: 4, result: web3.utils.toHex(cbor.encode(parsedJson)) });
-    console.log('result', cborEncoded);
   } else if (value.isEditing && !value.encodings.cbor) {
-    console.log('blank 4: CBOR');
     response.push({ id: 4, result: 0 });
   }
 
-  // type 8: the URI:
+  // type 8: the URI straight
   if (value.encodings.uri) {
-    console.log('adding 8: URI', value.uri);
     response.push({ id: 8, result: web3.utils.toHex(value.uri) });
   } else if (value.isEditing && !value.encodings.uri) {
-    console.log('blank 8: URI');
     response.push({ id: 8, result: 0 });
   }
 
-  // Let's Multicall!
-  console.log('starting multicall!');
+  // prepare multicall methods array
   response.forEach((call) => {
-    console.log('adding to multicall:', call.id);
     multiCallMethods.push(
       definitiveResolver.methods['setABI(bytes32,uint256,bytes)'](
         namehash(domain), call.id, call.result,
@@ -369,17 +353,13 @@ const setContractAbi = (resolverAddress, domain, value) => async (dispatch) => {
   // make the call!
   const accounts = await window.ethereum.enable();
   const currentAddress = accounts[0];
-  console.log('sending the message');
   return definitiveResolver.methods.multicall(multiCallMethods)
     .send({ from: currentAddress }, (e, result) => {
-      console.log('method sent', multiCallMethods);
       if (e) {
-        console.log('error in send');
         return dispatch(errorSetContent(CONTRACT_ABI, e.message));
       }
 
       const transactionConfirmed = () => () => {
-        console.log('transaction Confirmed!', result);
         dispatch(receiveSetContent(
           CONTRACT_ABI, result, response, (value.inputMethod === 'delete'),
         ));
