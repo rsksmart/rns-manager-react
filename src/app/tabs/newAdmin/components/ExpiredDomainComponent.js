368ffd7b72d5f5a8e5b42de26e515aefedc33de3
Jesse Clark <hello@developerjesse.com>
2020-06-17 19:00:24 +0300

    Missing semi.

--- src/app/tabs/newAdmin/components/ExpiredDomainComponent.js
+++ src/app/tabs/newAdmin/components/ExpiredDomainComponent.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { multilanguage } from 'redux-multilanguage'
+import { multilanguage } from 'redux-multilanguage';
 import propTypes from 'prop-types';
 
 import {
