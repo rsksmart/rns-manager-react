4adaf949f9dd1bd69b7b1819f01bd97490ca63aa
Jesse Clark <hello@developerjesse.com>
2021-03-24 18:36:47 +0200

    Mock textencoder/decoder. Reference: https://stackoverflow.com/questions/57712235/referenceerror-textencoder-is-not-defined-when-running-react-scripts-test

--- tests/setupTests.js
+++ tests/setupTests.js
@@ -3,3 +3,9 @@ var enzyme = require('enzyme');
 var Adapter = require('enzyme-adapter-react-16');
 
 enzyme.configure({ adapter: new Adapter() });
+
+// mocking TextEncoder/TextDecoder
+const { TextEncoder, TextDecoder } = require('util');
+
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
