d8eb616afdc65864178076bf9ba456517978405c
Jesse Clark <hello@developerjesse.com>
2021-04-16 23:44:29 +0300

    Add integrations to homepage (#409)
    * Create carousel using react-bootstrap.
    
    * Refactor to take wallets as a prop. Add test.
    
    * fix proptypes.
    
    * Update test to use store.
    
    * Add strings.
    
    * Update wallets/defi to be integrations.
    
    * Update language strings.

---
+++ src/app/tabs/home/components/WalletCarousel.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import { multilanguage } from 'redux-multilanguage';
+import propTypes from 'prop-types';
+import { Carousel, Row, Col } from 'react-bootstrap';
+
+const WalletCarousel = ({ wallets, strings }) => {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex) => {
+    setIndex(selectedIndex);
+  };
+
+  /**
+   * Group the wallets into 3s:
+   */
+  const walletsByThree = [];
+  wallets.forEach((wallet, i) => {
+    if (i % 3 === 0) {
+      // new set
+      walletsByThree[Math.floor(i / 3)] = [wallet];
+    } else {
+      walletsByThree[Math.floor(i / 3)].push(wallet);
+    }
+  });
+
+  const SingleItem = item => (
+    <Col key={item.name}>
+      <a href={item.link} target="_blank" rel="noopener noreferrer">
+        <div className="image-container" style={{ backgroundImage: `url(${item.image})` }} />
+        <p>{item.name}</p>
+      </a>
+    </Col>
+  );
+
+  return (
+    <div className="supported-wallets">
+      <h2>{strings.rns_integrations}</h2>
+      <Carousel activeIndex={index} onSelect={handleSelect} controls className="wallet-carousel">
+        {walletsByThree.map(walletGroup => (
+          <Carousel.Item key={walletGroup[0].name}>
+            <Row>
+              {walletGroup.map(wallet => SingleItem(wallet))}
+            </Row>
+          </Carousel.Item>
+        ))}
+      </Carousel>
+    </div>
+  );
+};
+
+WalletCarousel.propTypes = {
+  wallets: propTypes.arrayOf(propTypes.shape({
+    name: propTypes.string.isRequired,
+    link: propTypes.string.isRequired,
+    image: propTypes.string.isRequired,
+  })).isRequired,
+  strings: propTypes.shape({
+    rns_integrations: propTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default multilanguage(WalletCarousel);
---
+++ src/app/tabs/home/components/WalletCarousel.test.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { Provider } from 'react-redux';
+import { mockStoreEnglish } from '../../../../../tests/config/mockStore';
+import WalletCarousel from './WalletCarousel';
+
+describe('WalletCarousel', () => {
+  const wallet = i => ({ name: `name${i}`, image: `image${i}.jpg`, link: `url${i}` });
+  const wallets2 = [wallet(1), wallet(2)];
+  const wallets7 = [wallet(1), wallet(2), wallet(3), wallet(4), wallet(5), wallet(6), wallet(7)];
+
+  const store = mockStoreEnglish();
+  const generateComponent = wallets => (
+    <Provider store={store}><WalletCarousel wallets={wallets} /></Provider>
+  );
+
+  describe('basic', () => {
+    it('renders', () => {
+      const component = mount(generateComponent(wallets2));
+      expect(component).toBeDefined();
+    });
+
+    it('has header', () => {
+      const component = mount(generateComponent(wallets2));
+      expect(component.find('h2').text()).toBe('Wallet and dapp integrations');
+    });
+  });
+
+  describe('content', () => {
+    it('displays the correct content for first item', () => {
+      const component = mount(generateComponent(wallets2));
+
+      const first = component.find('.carousel-item').find('.col').at(0);
+
+      expect(first.find('p').text()).toBe('name1');
+      expect(first.find('a').props().href).toBe('url1');
+      expect(first.find('.image-container').props().style).toMatchObject({ backgroundImage: 'url(image1.jpg)' });
+    });
+
+    it('displays name of sixth item', () => {
+      const component = mount(generateComponent(wallets7));
+      const column = component.find('.carousel-item').at(1).find('.col').at(2);
+      expect(column.find('p').text()).toBe('name6');
+    });
+  });
+
+  describe('number of items', () => {
+    it('creates 3 pages', () => {
+      const component = mount(generateComponent(wallets7));
+      expect(component.find('.carousel-item')).toHaveLength(3);
+    });
+
+    it('creates 1 page', () => {
+      const component = mount(generateComponent(wallets2));
+      expect(component.find('.carousel-item')).toHaveLength(1);
+    });
+  });
+});
