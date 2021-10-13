37bf9c4fbff9766dc35c6323c970482277ed9a84
Ilan <ilanolkies@outlook.com>
2021-10-12 23:11:08 -0300

    Add Trezor and DCent + update rLogin

--- src/app/rLogin/rLogin.js
+++ src/app/rLogin/rLogin.js
@@ -4,23 +4,29 @@ import WalletConnectProvider from '@walletconnect/web3-provider';
 import Portis from '@portis/web3';
 import { ledgerProviderOptions } from '@rsksmart/rlogin-ledger-provider';
 
+const rpcUrls = process.env.REACT_APP_ENVIRONMENT_ID === '30' ? {
+  30: 'https://public-node.rsk.co',
+} : {
+  31: 'https://public-node.testnet.rsk.co',
+}
+
 const rLogin = new RLogin({
   cachedProvider: false,
   providerOptions: {
     walletconnect: {
       package: WalletConnectProvider,
       options: {
-        rpc: {
-          30: 'https://public-node.rsk.co',
-          31: 'https://public-node.testnet.rsk.co',
-        },
+        rpc: rpcUrls
       },
     },
     portis: {
       package: Portis,
       options: {
         id: '7ea0e47e-ff3c-4fc7-85cb-7b336d0569ed',
-        network: {
+        network: process.env.REACT_APP_ENVIRONMENT_ID === '30' ? {
+          nodeUrl: 'https://public-node.rsk.co',
+          chainId: 30,
+        } : {
           nodeUrl: 'https://public-node.testnet.rsk.co',
           chainId: 31,
         },
@@ -28,13 +34,20 @@ const rLogin = new RLogin({
     },
     'custom-ledger': {
       ...ledgerProviderOptions,
-      options: {
-        rpcUrl: 'https://public-node.testnet.rsk.co',
-        chainId: 31,
-      },
     },
+    'custom-dcent': {
+      ...dcentProviderOptions,
+    },
+    'custom-trezor': {
+      ...trezorProviderOptions,
+      options: {
+        manifestEmail: 'info@iovlabs.org',
+        manifestAppUrl: process.env.REACT_APP_URL,
+      }
+    }
   },
-  supportedChains: [parseInt(process.env.REACT_APP_ENVIRONMENT_ID)],
+  rpcUrls,
+  supportedChains: Object.keys(rpcUrls).map(Number),
 });
 
 export default rLogin;
