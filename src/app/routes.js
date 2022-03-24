b8f36aa31e99aa4c9a56daf8588aac14ff363014
Jesse Clark <hello@developerjesse.com>
2022-03-24 15:34:54 +0200

    Add route for GH pages route.

--- src/app/routes.js
+++ src/app/routes.js
@@ -24,6 +24,8 @@ const Routes = (props) => {
         !hasContracts && <Route component={ErrorTab} />
       }
       <Route exact path="/" component={HomeTab} />
+      {/* the following path is only for GitHub pages */}
+      <Route exact path="/rns-manager-react" component={HomeTab} />
       <Route path="/search" component={SearchTab} />
       <Route path="/resolve" component={ResolveTab} />
       <Route path="/faq" component={FaqTab} />
