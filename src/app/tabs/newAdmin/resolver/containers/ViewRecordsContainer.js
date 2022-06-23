8b67f4783e65a51b8a0fb739c918fa4b8eaf3651
Evelina <gabriela.buiciag@iovlabs.org>
2022-06-23 18:47:17 +0300

    filtering out the text record resolver at it has it own tab now

--- src/app/tabs/newAdmin/resolver/containers/ViewRecordsContainer.js
+++ src/app/tabs/newAdmin/resolver/containers/ViewRecordsContainer.js
@@ -1,11 +1,12 @@
 import { connect } from 'react-redux';
 import { ViewRecordsComponent } from '../components';
-import { CONTENT_HASH } from '../types';
+import { CONTENT_HASH, TEXT_RECORD } from '../types';
 
 const mapStateToProps = state => ({
   domain: state.auth.name,
   resolverAddr: state.newAdmin.resolver.resolverAddr,
-  content: Object.entries(state.newAdmin.resolver.content).filter(c => c[0] !== CONTENT_HASH),
+  content: Object.entries(state.newAdmin.resolver.content)
+    .filter(c => c[0] !== CONTENT_HASH && c[0] !== TEXT_RECORD),
   gettingContent: state.newAdmin.resolver.gettingContent,
 });
 
