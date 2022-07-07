60363fd012915cca644ae061e925893bd47ec2ca
Evelina <gabriela.buiciag@iovlabs.org>
2022-07-07 15:39:47 +0300

    PR addresssed the App checks first the domain resolver to see if text records are a supportedInterface and if not, lets the user know.

--- src/app/tabs/newAdmin/textrecords/components/TextRecordsComponent.js
+++ src/app/tabs/newAdmin/textrecords/components/TextRecordsComponent.js
@@ -9,31 +9,41 @@ import {
 } from '../containers';
 
 const TextRecordsComponent = ({
-  gettingResolver, strings,
+  gettingResolver, strings, resolverName,
 }) => {
   if (gettingResolver) {
     return <UserWaitingComponent />;
   }
-
   return (
-    <div className="resolver">
-      <h1>{strings.text_records}</h1>
-      <p>{strings.text_records_explanation}</p>
-      <p><strong>{strings.text_records_local}</strong></p>
-      <ViewGlobalRecordsContainer />
-      <ViewCustomRecordsContainer />
-      <SearchTextRecordContainer />
-      <NewTextRecordContainer />
-    </div>
+    <>
+      <div className="resolver">
+        <h1>{strings.text_records}</h1>
+        <p>{strings.text_records_explanation}</p>
+        {resolverName === 'DEFINITIVE_RESOLVER' && (
+          <>
+            <p><strong>{strings.text_records_local}</strong></p>
+            <ViewGlobalRecordsContainer />
+            <ViewCustomRecordsContainer />
+            <SearchTextRecordContainer />
+            <NewTextRecordContainer />
+          </>
+        )}
+        {resolverName !== 'DEFINITIVE_RESOLVER' && (
+          <h1><italic>{strings.definitive_resolver_text_records}</italic></h1>
+        )}
+      </div>
+    </>
   );
 };
 
 TextRecordsComponent.propTypes = {
+  resolverName: propTypes.string.isRequired,
   gettingResolver: propTypes.bool.isRequired,
   strings: propTypes.shape({
     text_records: propTypes.string.isRequired,
     text_records_explanation: propTypes.string.isRequired,
     text_records_local: propTypes.string.isRequired,
+    definitive_resolver_text_records: propTypes.string.isRequired,
   }).isRequired,
 };
 
