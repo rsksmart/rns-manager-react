fee94781eb76b5c01ad38409bd0c23736ccf9883
Sergio <sergio.jara@iovlabs.org>
2022-11-22 17:28:26 -0300

    feat: partner registrar integration

--- src/app/rLogin/rLogin.js
+++ src/app/rLogin/rLogin.js
@@ -5,13 +5,16 @@ const trezorOptions = {
   manifestAppUrl: process.env.REACT_APP_URL,
 };
 
-const isMainnet = process.env.REACT_APP_ENVIRONMENT_ID === '30';
-
-const rpcUrls = isMainnet ? {
-  30: 'https://public-node.rsk.co',
-} : {
-  31: 'https://public-node.testnet.rsk.co',
+export const rpcUrlSelector = (network) => {
+  switch (network) {
+    case '30': return 'https://public-node.rsk.co';
+    case '31': return 'https://public-node.testnet.rsk.co';
+    case process.env.REACT_APP_ENVIRONMENT_ID: return 'http://localhost:8545';
+    default: return 'invalid';
+  }
 };
+
+const rpcUrls = rpcUrlSelector(process.env.REACT_APP_ENVIRONMENT_ID);
 
 const rLogin = createRLogin(rpcUrls, trezorOptions);
 
