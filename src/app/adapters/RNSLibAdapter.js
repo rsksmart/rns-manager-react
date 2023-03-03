6579bbe3584e4673d04c28bdf55c885a584bebcb
Sergio <sergio.jara@iovlabs.org>
2023-03-03 09:59:31 -0300

    fix: sonarcloud

--- src/app/adapters/RNSLibAdapter.js
+++ src/app/adapters/RNSLibAdapter.js
@@ -1,19 +1,8 @@
 import { rns as registry } from './configAdapter';
 
 // eslint-disable-next-line import/prefer-default-export
-export const getOptions = () => {
-  switch (process.env.REACT_APP_ENVIRONMENT) {
-    case 'local':
-    case 'testing':
-      return {
-        contractAddresses: {
-          registry,
-        },
-      };
-    default: return {
-      contractAddresses: {
-        registry,
-      },
-    };
-  }
-};
+export const getOptions = () => ({
+  contractAddresses: {
+    registry,
+  },
+});
