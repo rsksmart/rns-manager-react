2d3bdf3cca3c77217b9d664d399873914186561e
Diego Rosales <diego.rosales@iovlabs.org>
2023-03-23 14:54:12 -0600

    feat: added custom font support

--- src/app/helpers/theme.js
+++ src/app/helpers/theme.js
@@ -3,6 +3,8 @@ import themeConfig from '../../theme.json';
 import defaultLogo from '../../assets/img/logo.svg';
 import defaultSpacer from '../../assets/img/home/bottom-background.svg';
 
+const defaultFont = "'Rubik', sans-serif";
+
 function valueOrDefault(prop, defaultProp) {
   if (prop && prop.trim() !== '') return prop;
   return defaultProp;
@@ -13,10 +15,27 @@ const theme = {
   images: {
     homeSpacer: valueOrDefault(themeConfig.images.homeSpacer, defaultSpacer),
   },
+  font: {
+    family: valueOrDefault(themeConfig.font.family, defaultFont),
+    src: valueOrDefault(themeConfig.font.src, ''),
+  },
 };
 
 export const setupCssTheme = () => {
+  document.documentElement.style.setProperty('--primary-font', theme.font.family);
   document.documentElement.style.setProperty('--background-home-spacer', `url("${theme.images.homeSpacer}")`);
+
+  // Load font
+  if (theme.font.src !== '') {
+    const font = new FontFace(theme.font.family, `url(${theme.font.src})`);
+    font.load()
+      .then((loadedFF) => {
+        document.fonts.add(loadedFF);
+      })
+      .catch(() => {
+        console.warn('THEME: Failed to load custom font.');
+      });
+  }
 };
 
 export default theme;
