4184c53924dcb04a428c39154dca8690ba225002
LordCheta <chinaechetam.maduabuchi@iovlabs.org>
2023-04-06 10:16:39 +0100

    fix: update contentHash function
    -S -m "fix: fix merge conflicts"

--- src/app/helpers/contentHash.js
+++ src/app/helpers/contentHash.js
@@ -2,6 +2,35 @@ import {ethers} from 'ethers';
 import {namehash} from 'ethers/lib/utils';
 import getSigner from './getSigner';
 import getProvider from './getProvider';
+import contentHash from 'content-hash';
+
+const decodeContenthash = (encoded) => {
+  let decoded = '';
+  let protocolType = '';
+
+  try {
+    decoded = contentHash.decode(encoded);
+    const codec = contentHash.getCodec(encoded);
+    if (codec === 'ipfs-ns') {
+      protocolType = 'ipfs';
+    } else if (codec === 'ipns-ns') {
+      decoded = bs58.decode(decoded).slice(2).toString();
+      protocolType = 'ipns';
+    } else if (codec === 'swarm-ns') {
+      protocolType = 'bzz';
+    } else if (codec === 'onion') {
+      protocolType = 'onion';
+    } else if (codec === 'onion3') {
+      protocolType = 'onion3';
+    } else {
+      throw new Error("UNSUPPORTED_CONTENTHASH_PROTOCOL");
+    }
+  } catch (e) {
+    throw new Error("UNSUPPORTED_CONTENTHASH_PROTOCOL");
+  }
+
+  return { decoded, protocolType };
+}
 
 export const setContentHash = async (domain, content, resolverAddress, definitiveResolverAbi) => {
   const signer = await getSigner();
@@ -16,10 +45,29 @@ export const setContentHash = async (domain, content, resolverAddress, definitiv
 };
 
 export const contentHash = async (resolverAddress, domain, definitiveResolverAbi) => {
-  const resolver = new ethers.Contract(resolverAddress, definitiveResolverAbi, getProvider());
+  const CONTENTHASH_INTERFACE = '0xbc1c58d1';
+  const signer = await getSigner();
+  const resolver = new ethers.Contract(resolverAddress, definitiveResolverAbi, signer);
   const hash = namehash(domain);
   try {
-    return await resolver.contenthash(hash);
+    const supportsInterface = await resolver.supportsInterface(CONTENTHASH_INTERFACE);
+
+    if (!supportsInterface) {
+      throw new Error("NO_CONTENTHASH_INTERFACE");
+    }
+    const resultEncoded = await resolver.contenthash(hash);
+
+    if(!resultEncoded || resultEncoded === '0x') {
+      throw new Error("NO_CONTENTHASH_SET");
+    }
+
+    const resultDecoded = decodeContenthash(resultEncoded);
+
+    if(!resultDecoded?.protocolType) {
+      throw new Error("UNSUPPORTED_CONTENTHASH_PROTOCOL");
+    }
+
+    return resultDecoded;
   } catch (error) {
     return error;
   }
