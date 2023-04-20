2d53762aed4bef65a11dfeee0d18cc3b35c38155
LordCheta <chinaechetam.maduabuchi@iovlabs.org>
2023-04-20 01:09:31 +0100

    lint: fix lint issues

--- src/app/tabs/resolve/operations.js
+++ src/app/tabs/resolve/operations.js
@@ -1,16 +1,16 @@
-import { hash as namehash } from "@ensdomains/eth-ens-namehash";
-import { isValidAddress } from "rskjs-util";
-import { formatsByCoinType } from "@ensdomains/address-encoder";
-import { ethers } from "ethers";
-import * as actions from "./actions";
-import { rnsAbi, abstractResolverAbi } from "./abis.json";
-import { definitiveResolverAbi } from "../newAdmin/resolver/definitiveAbis.json";
-import { rns as rnsAddress } from "../../adapters/configAdapter";
-import resolverInterfaces from "./resolverInterfaces.json";
-import { ERROR_RESOLVE_NAME } from "./types";
-import { ERROR_SAME_VALUE, EMPTY_ADDRESS } from "../newAdmin/types";
-import { rns, defaultSigner } from "../../rns-sdk";
-import { contentHash as CH } from "../../helpers/contentHash";
+import { hash as namehash } from '@ensdomains/eth-ens-namehash';
+import { isValidAddress } from 'rskjs-util';
+import { formatsByCoinType } from '@ensdomains/address-encoder';
+import { ethers } from 'ethers';
+import * as actions from './actions';
+import { rnsAbi, abstractResolverAbi } from './abis.json';
+import { definitiveResolverAbi } from '../newAdmin/resolver/definitiveAbis.json';
+import { rns as rnsAddress } from '../../adapters/configAdapter';
+import resolverInterfaces from './resolverInterfaces.json';
+import { ERROR_RESOLVE_NAME } from './types';
+import { ERROR_SAME_VALUE, EMPTY_ADDRESS } from '../newAdmin/types';
+import { rns, defaultSigner } from '../../rns-sdk';
+import { contentHash as CH } from '../../helpers/contentHash';
 
 /**
  * Resolves a domain name using the js library
@@ -19,33 +19,31 @@ import { contentHash as CH } from "../../helpers/contentHash";
  * @param {function} errorFunction the function to dispatch if there is an error
  * @param {string} value the value to check the resolution against to see if they match
  */
-export const resolveDomain =
-  (domain, errorFunction = null, value = null) =>
-  async (dispatch) => {
-    const rnsSdk = rns();
+export const resolveDomain = (domain, errorFunction = null, value = null) => async (dispatch) => {
+  const rnsSdk = rns();
 
-    dispatch(actions.requestAddr());
+  dispatch(actions.requestAddr());
 
-    try {
-      const response = await rnsSdk.getResolver(domain);
+  try {
+    const response = await rnsSdk.getResolver(domain);
 
-      if (value && response.toLowerCase() === value.toLowerCase()) {
-        dispatch(errorFunction(ERROR_SAME_VALUE));
-        return false;
-      }
-
-      dispatch(actions.receiveAddr(response));
-      return response.toLowerCase();
-    } catch (error) {
-      dispatch(actions.errorResolve(error));
-      dispatch(errorFunction(ERROR_RESOLVE_NAME));
+    if (value && response.toLowerCase() === value.toLowerCase()) {
+      dispatch(errorFunction(ERROR_SAME_VALUE));
       return false;
     }
-  };
 
-export const identifyInterfaces = (domain) => async (dispatch) => {
+    dispatch(actions.receiveAddr(response));
+    return response.toLowerCase();
+  } catch (error) {
+    dispatch(actions.errorResolve(error));
+    dispatch(errorFunction(ERROR_RESOLVE_NAME));
+    return false;
+  }
+};
+
+export const identifyInterfaces = domain => async (dispatch) => {
   if (!domain) {
-    return dispatch(actions.receiveResolve(""));
+    return dispatch(actions.receiveResolve(''));
   }
 
   dispatch(actions.requestResolve());
@@ -56,13 +54,13 @@ export const identifyInterfaces = (domain) => async (dispatch) => {
 
   try {
     const resolverAddress = await rnsContract.resolver(hash);
-    if (resolverAddress === "0x0000000000000000000000000000000000000000") {
-      return dispatch(actions.errorResolve("this name is not registered"));
+    if (resolverAddress === '0x0000000000000000000000000000000000000000') {
+      return dispatch(actions.errorResolve('this name is not registered'));
     }
     const abstractResolver = new ethers.Contract(
       resolverAddress,
       abstractResolverAbi,
-      defaultSigner
+      defaultSigner,
     );
     dispatch(actions.receiveResolverAddress(resolverAddress));
 
@@ -72,7 +70,7 @@ export const identifyInterfaces = (domain) => async (dispatch) => {
     for (let i = 0; i < resolverInterfaces.length; i += 1) {
       const resolverInterface = resolverInterfaces[i];
       const supportsInterface = await abstractResolver.supportsInterface(
-        resolverInterface.signature
+        resolverInterface.signature,
       );
       if (supportsInterface) {
         resolutions.push(resolverInterface);
@@ -84,7 +82,7 @@ export const identifyInterfaces = (domain) => async (dispatch) => {
       return Promise.all(resolutions).then(() => dispatch(actions.receiveResolve()));
     }
 
-    return dispatch(actions.errorResolve("no resolution found"));
+    return dispatch(actions.errorResolve('no resolution found'));
   } catch (error) {
     return dispatch(actions.errorResolve(error.message));
   }
@@ -96,7 +94,7 @@ export const addr = (resolverAddress, name) => async (dispatch) => {
   const addrResolver = new ethers.Contract(
     resolverAddress,
     resolverInterfaces[0].abi,
-    defaultSigner
+    defaultSigner,
   );
 
   const hash = namehash(name);
@@ -110,59 +108,57 @@ export const addr = (resolverAddress, name) => async (dispatch) => {
   }
 };
 
-export const chainAddr =
-  (resolverAddress, name, chainId) => async (dispatch) => {
-    dispatch(actions.requestChainAddr());
+export const chainAddr = (resolverAddress, name, chainId) => async (dispatch) => {
+  dispatch(actions.requestChainAddr());
 
-    const addrResolver = new ethers.Contract(
-      resolverAddress,
-      resolverInterfaces[1].abi,
-      defaultSigner
-    );
+  const addrResolver = new ethers.Contract(
+    resolverAddress,
+    resolverInterfaces[1].abi,
+    defaultSigner,
+  );
 
-    const hash = namehash(name);
+  const hash = namehash(name);
 
-    try {
-      const chainAddrResolution = await addrResolver.chainAddr(hash, chainId);
+  try {
+    const chainAddrResolution = await addrResolver.chainAddr(hash, chainId);
 
-      return dispatch(actions.receiveChainAddr(chainAddrResolution));
-    } catch (error) {
-      return dispatch(actions.errorChainAddr(error.message));
+    return dispatch(actions.receiveChainAddr(chainAddrResolution));
+  } catch (error) {
+    return dispatch(actions.errorChainAddr(error.message));
+  }
+};
+
+export const multicoin = (resolverAddress, domain, chainId) => async (dispatch) => {
+  dispatch(actions.requestChainAddr());
+
+  const hash = namehash(domain);
+
+  const resolver = new ethers.Contract(
+    resolverAddress,
+    definitiveResolverAbi,
+    defaultSigner,
+  );
+
+  try {
+    const resolution = await resolver.addr(hash, chainId);
+
+    if (!resolution || resolution === EMPTY_ADDRESS) {
+      return dispatch(actions.receiveChainAddr(''));
     }
-  };
 
-export const multicoin =
-  (resolverAddress, domain, chainId) => async (dispatch) => {
-    dispatch(actions.requestChainAddr());
+    // eslint-disable-next-line new-cap
+    const dataBuffer = new Buffer.from(resolution.replace('0x', ''), 'hex');
+    const result = formatsByCoinType[chainId].encoder(dataBuffer);
 
-    const hash = namehash(domain);
-
-    const resolver = new ethers.Contract(
-      resolverAddress,
-      definitiveResolverAbi,
-      defaultSigner
-    );
-
-    try {
-      const resolution = await resolver.addr(hash, chainId);
-
-      if (!resolution || resolution === EMPTY_ADDRESS) {
-        return dispatch(actions.receiveChainAddr(""));
-      }
-
-      // eslint-disable-next-line new-cap
-      const dataBuffer = new Buffer.from(resolution.replace("0x", ""), "hex");
-      const result = formatsByCoinType[chainId].encoder(dataBuffer);
-
-      if (chainId === "0x80000089") {
-        dispatch(actions.receiveAddr(result));
-      }
-
-      return dispatch(actions.receiveChainAddr(result));
-    } catch (error) {
-      return dispatch(actions.errorChainAddr(error.message));
+    if (chainId === '0x80000089') {
+      dispatch(actions.receiveAddr(result));
     }
-  };
+
+    return dispatch(actions.receiveChainAddr(result));
+  } catch (error) {
+    return dispatch(actions.errorChainAddr(error.message));
+  }
+};
 
 export const name = (resolverAddress, address) => async (dispatch) => {
   dispatch(actions.requestName());
@@ -170,11 +166,11 @@ export const name = (resolverAddress, address) => async (dispatch) => {
   const nameResolver = new ethers.Contract(
     resolverAddress,
     resolverInterfaces[2].abi,
-    defaultSigner
+    defaultSigner,
   );
 
   const value = isValidAddress(address)
-    ? `${address.replace("0x", "")}.addr.reverse`
+    ? `${address.replace('0x', '')}.addr.reverse`
     : address;
   const hash = namehash(value);
 
@@ -187,45 +183,44 @@ export const name = (resolverAddress, address) => async (dispatch) => {
   }
 };
 
-export const contentHash = (domain) => async (dispatch) => {
-  dispatch(actions.requestContent("CONTENT_HASH"));
+export const contentHash = domain => async (dispatch) => {
+  dispatch(actions.requestContent('CONTENT_HASH'));
 
   try {
     const rnsContract = new ethers.Contract(rnsAddress, rnsAbi, defaultSigner);
     const hash = namehash(domain);
     const resolverAddress = await rnsContract.resolver(hash);
     const result = await CH(resolverAddress, domain, definitiveResolverAbi);
-    return dispatch(actions.receiveContent("CONTENT_HASH", result));
+    return dispatch(actions.receiveContent('CONTENT_HASH', result));
   } catch (error) {
-    return dispatch(actions.errorContent("CONTENT_HASH", error.message));
+    return dispatch(actions.errorContent('CONTENT_HASH', error.message));
   }
 };
 
-export const searchAddressOrDomain = (input) => (dispatch) => {
+export const searchAddressOrDomain = input => (dispatch) => {
   const value = isValidAddress(input)
-    ? `${input.replace("0x", "")}.addr.reverse`
+    ? `${input.replace('0x', '')}.addr.reverse`
     : input;
   return dispatch(identifyInterfaces(value));
 };
 
-export const getAddress =
-  (resolverAddress, supportedInterfaces, domain, chainId) => (dispatch) => {
-    supportedInterfaces.forEach((interfaceId) => {
-      switch (interfaceId) {
-        case "multicoin":
-          return dispatch(
-            multicoin(resolverAddress, domain, chainId || "0x80000089")
-          );
-        case "chainAddr":
-          return dispatch(
-            chainAddr(resolverAddress, domain, chainId || "0x80000089")
-          );
-        case "addr":
-          return dispatch(addr(resolverAddress, domain));
-        case "contenthash":
-          return dispatch(contentHash(domain));
-        default:
-          return false;
-      }
-    });
-  };
+export const getAddress = (resolverAddress, supportedInterfaces, domain, chainId) => (dispatch) => {
+  supportedInterfaces.forEach((interfaceId) => {
+    switch (interfaceId) {
+      case 'multicoin':
+        return dispatch(
+          multicoin(resolverAddress, domain, chainId || '0x80000089'),
+        );
+      case 'chainAddr':
+        return dispatch(
+          chainAddr(resolverAddress, domain, chainId || '0x80000089'),
+        );
+      case 'addr':
+        return dispatch(addr(resolverAddress, domain));
+      case 'contenthash':
+        return dispatch(contentHash(domain));
+      default:
+        return false;
+    }
+  });
+};
