c2a31ece126560b3e3946c3519ca1877af6725b6
Andrés Quijano <andres.quijano@iovlabs.org>
2024-02-13 10:11:17 -0500

    fix: change error message when user rejects transaction

--- src/app/components/UserErrorComponent.js
+++ src/app/components/UserErrorComponent.js
@@ -10,32 +10,42 @@ import { TRANSACTION_RECEIPT_FAILED } from '../types';
 import closeRed from '../../assets/img/close-red.svg';
 
 const UserErrorComponent = ({
-  title, message, handleCloseClick, strings, visible,
+  title,
+  message,
+  handleCloseClick,
+  strings,
+  visible,
 }) => {
   if (!visible) {
-    return (<></>);
+    return <></>;
   }
 
   const formatMessage = () => {
+    if (message.toLowerCase().startsWith('user rejected transaction')) {
+      return strings.user_rejected_transaction;
+    }
     switch (message) {
-      case ERROR_SAME_VALUE: return strings.same_value;
-      case ERROR_RESOLVE_NAME: return strings.resolve_not_set;
-      case ADDRESS_ENCODING_ERROR: return strings.could_not_encode_address;
-      case TRANSACTION_RECEIPT_FAILED: return strings.transaction_receipt_failed;
-      default: return message;
+      case ERROR_SAME_VALUE:
+        return strings.same_value;
+      case ERROR_RESOLVE_NAME:
+        return strings.resolve_not_set;
+      case ADDRESS_ENCODING_ERROR:
+        return strings.could_not_encode_address;
+      case TRANSACTION_RECEIPT_FAILED:
+        return strings.transaction_receipt_failed;
+      default:
+        return message;
     }
   };
 
   return (
     <div className="error">
-      <button
-        type="button"
-        className="close"
-        onClick={handleCloseClick}
-      >
+      <button type="button" className="close" onClick={handleCloseClick}>
         <img src={closeRed} alt={strings.close} />
       </button>
-      <p><strong>{title}</strong></p>
+      <p>
+        <strong>{title}</strong>
+      </p>
       <p>{formatMessage()}</p>
     </div>
   );
@@ -55,6 +65,7 @@ UserErrorComponent.propTypes = {
     resolve_not_set: propTypes.string.isRequired,
     could_not_encode_address: propTypes.string.isRequired,
     transaction_receipt_failed: propTypes.string.isRequired,
+    user_rejected_transaction: propTypes.string.isRequired,
   }).isRequired,
   title: propTypes.string,
   message: propTypes.string,
