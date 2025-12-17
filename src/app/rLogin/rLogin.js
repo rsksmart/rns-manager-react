2028d33a02736feecb5d0d13ffd1a6e7cb4962fa
Jesse Clark <hello@developerjesse.com>
2025-12-17 12:41:00 +0200

    Remove all provider options
    - Walletconnect was not working and that bridge URL is no longer working
    - remove second set of () around RLogin

--- src/app/rLogin/rLogin.js
+++ src/app/rLogin/rLogin.js
@@ -10,6 +10,7 @@ const createRLogin = () => {
     31: 'https://public-node.testnet.rsk.co',
   };
 
+  /*
   const providerOptions = Object.assign({
     walletconnect: {
       package: WalletConnectProvider,
@@ -18,24 +19,25 @@ const createRLogin = () => {
         bridge: 'https://walletconnect-bridge.rifos.org/',
       },
     },
-    /* 'custom-ledger': ledgerProviderOptions,
+    'custom-ledger': ledgerProviderOptions,
     'custom-trezor': {
       ...trezorProviderOptions,
       options: {
         manifestEmail: 'info@iovlabs.org',
         manifestAppUrl: process.env.REACT_APP_URL,
       },
-    }, */
+    },
   });
+   */
 
   const supportedChains = Object.keys(rpcUrls).map(Number);
 
-  return new RLogin(({
+  return new RLogin({
     cacheProvider: false,
-    providerOptions,
+    providerOptions: undefined,
     rpcUrls,
     supportedChains,
-  }));
+  });
 };
 
 export default createRLogin();
