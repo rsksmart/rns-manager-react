bc14b9ea5e08dd7c1947592669eb1ff726224276
Jesse Clark <hello@developerjesse.com>
2025-12-17 12:31:20 +0200

    Remove hardware wallets right now and use only metamask

--- src/app/rLogin/rLogin.js
+++ src/app/rLogin/rLogin.js
@@ -1,18 +1,41 @@
-import { createRLogin } from '@rsksmart/rlogin-essentials';
+import RLogin from '@rsksmart/rlogin';
+import WalletConnectProvider from '@walletconnect/web3-provider';
+// import { trezorProviderOptions } from '@rsksmart/rlogin-trezor-provider';
+// import { ledgerProviderOptions } from '@rsksmart/rlogin-ledger-provider';
 
-const trezorOptions = {
-  manifestEmail: 'info@iovlabs.org',
-  manifestAppUrl: process.env.REACT_APP_URL,
+const createRLogin = () => {
+  const rpcUrls = process.env.REACT_APP_ENVIRONMENT_ID === '30' ? {
+    30: 'https://public-node.rsk.co',
+  } : {
+    31: 'https://public-node.testnet.rsk.co',
+  };
+
+  const providerOptions = Object.assign({
+    walletconnect: {
+      package: WalletConnectProvider,
+      options: {
+        rpcUrls,
+        bridge: 'https://walletconnect-bridge.rifos.org/',
+      },
+    },
+    /* 'custom-ledger': ledgerProviderOptions,
+    'custom-trezor': {
+      ...trezorProviderOptions,
+      options: {
+        manifestEmail: 'info@iovlabs.org',
+        manifestAppUrl: process.env.REACT_APP_URL,
+      },
+    }, */
+  });
+
+  const supportedChains = Object.keys(rpcUrls).map(Number);
+
+  return new RLogin(({
+    cacheProvider: false,
+    providerOptions,
+    rpcUrls,
+    supportedChains,
+  }));
 };
 
-const isMainnet = process.env.REACT_APP_ENVIRONMENT_ID === '30';
-
-const rpcUrls = isMainnet ? {
-  30: 'https://public-node.rsk.co',
-} : {
-  31: 'https://public-node.testnet.rsk.co',
-};
-
-const rLogin = createRLogin(rpcUrls, trezorOptions);
-
-export default rLogin;
+export default createRLogin();
